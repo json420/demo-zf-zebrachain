81f6a1962e7b45de57ad464f90b00df85d3341d8
Jason Gerard DeRose <jasonderose@gmail.com>
2025-06-28 14:03:30 -0600

    More doodles on range request, but something is broken

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,13 @@ use zf_zebrachain::{BLOCK, ChainStore, Hash};
 const CHAIN_HASH: &[u8] = b"EWKP7KIM6RAB748D6VLT68BES58BHDOAQP8BS4FLBGPQ69ROF5HTDEHPPXWCGFCC";
 const TAIL_BLOCK_HASH: &[u8] = b"DEFCQZHUV67IN5AUP7SYIHCR8NZNRZ7STS5H6RVM4WWSGMUZSAUKTXDQWD7JXE6C";
 
-fn range_header(range: Range<usize>) -> HeaderValue {
+fn range_header(range: Range<u64>) -> HeaderValue {
     let value = format!("bytes={}-{}", range.start, range.end - 1);
+    HeaderValue::from_str(&value).unwrap()
+}
+
+fn range_header_continue(offset: u64) -> HeaderValue {
+    let value = format!("bytes={}-", offset);
     HeaderValue::from_str(&value).unwrap()
 }
 
@@ -18,10 +23,11 @@ fn main() {
     let client = reqwest::blocking::Client::new();
 
     let url = format!("https://json420.github.io/chains/{}", chain_hash);
+
     println!("Downloading chain from {url}");
     let response = client
-        .get(url)
-        .header(RANGE, range_header(0..BLOCK))
+        .get(&url)
+        .header(RANGE, range_header(0..BLOCK as u64))
         .send()
         .unwrap();
 
@@ -30,20 +36,38 @@ fn main() {
         println!("{key}: {value:?}");
     }
 
-    if response.status() == 206 {
-        let body = response.bytes().unwrap();
-        let mut chain = store
-            .create_chain(&body.slice(0..BLOCK), &chain_hash)
-            .unwrap();
-        for index in 1..body.len() / BLOCK {
-            chain
-                .append(&body.slice(index * BLOCK..(index + 1) * BLOCK))
-                .unwrap();
-        }
-        assert_eq!(
-            chain.tail().block_hash,
-            Hash::from_z32(TAIL_BLOCK_HASH).unwrap()
-        );
-        assert_eq!(chain.count(), 420);
+    if response.status() != 206 {
+        panic!("Expected 206");
     }
+    let body = response.bytes().unwrap();
+    let mut chain = store.create_chain(&body, &chain_hash).unwrap();
+
+    //let mut chain = store.open_chain(&chain_hash).unwrap();
+
+    println!("");
+    let response = client
+        .get(&url)
+        .header(RANGE, range_header_continue(BLOCK as u64))
+        .send()
+        .unwrap();
+
+    println!("status: {}", response.status());
+    for (key, value) in response.headers() {
+        println!("{key}: {value:?}");
+    }
+    if response.status() != 206 {
+        panic!("Expected 206");
+    }
+    for index in 0..body.len() / BLOCK {
+        println!("{index}");
+        chain
+            .append(&body.slice(index * BLOCK..(index + 1) * BLOCK))
+            .unwrap();
+    }
+    assert_eq!(
+        chain.tail().block_hash,
+        Hash::from_z32(TAIL_BLOCK_HASH).unwrap()
+    );
+    assert_eq!(chain.count(), 420);
+
 }
