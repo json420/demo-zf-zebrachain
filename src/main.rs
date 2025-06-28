431f953fda7e3afab6603b73486c2658b3548e9b
Jason Gerard DeRose <jasonderose@gmail.com>
2025-06-28 10:19:16 -0600

    Append all blocks in test chain and check final block hash

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use zf_zebrachain::{BLOCK, ChainStore, Hash};
 
 const CHAIN_HASH: &str = "EWKP7KIM6RAB748D6VLT68BES58BHDOAQP8BS4FLBGPQ69ROF5HTDEHPPXWCGFCC";
+const TAIL_BLOCK_HASH: &[u8] = b"DEFCQZHUV67IN5AUP7SYIHCR8NZNRZ7STS5H6RVM4WWSGMUZSAUKTXDQWD7JXE6C";
 
 fn main() {
     let tmpdir = tempfile::TempDir::new().unwrap();
@@ -12,7 +13,17 @@ fn main() {
 
     if response.status() == 200 {
         let body = response.bytes().unwrap();
-        let mut chain = store.create_chain(&body.slice(0..BLOCK), &chain_hash).unwrap();
-        println!("OK {}", body.len());
+        let mut chain = store
+            .create_chain(&body.slice(0..BLOCK), &chain_hash)
+            .unwrap();
+        for index in 1..body.len() / BLOCK {
+            chain
+                .append(&body.slice(index * BLOCK..(index + 1) * BLOCK))
+                .unwrap();
+        }
+        assert_eq!(
+            chain.tail().block_hash,
+            Hash::from_z32(TAIL_BLOCK_HASH).unwrap()
+        );
     }
 }
