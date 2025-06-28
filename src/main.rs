ef040767b0f6bbfaf5c5b10acb4c5429f68821f9
Jason Gerard DeRose <jasonderose@gmail.com>
2025-06-28 09:14:30 -0600

    Download chain file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
-    println!("Hello, world!");
+    let response = reqwest::blocking::get("https://json420.github.io/chains/EWKP7KIM6RAB748D6VLT68BES58BHDOAQP8BS4FLBGPQ69ROF5HTDEHPPXWCGFCC").unwrap();
+    println!("done");
 }
